208db04cc71f37cd5a8d4e7bbf1194830a75426a
Anti-Mage <jpottagarda@gmail.com>
2023-11-16 16:23:46 +0800

    Defined Vegetables_Class - Data Field, Constructors and Methods

---
+++ TOPIC1_OOP_BASICS/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TOPIC1_OOP_BASICS;
+
+/**
+ *
+ * @author Admin
+ */
+public class Main {
+    
+     public static void main(String[] args) {
+        Vegetables Vegetables_01 = new Vegetables ("Utan ni", 342.98);
+        
+        Vegetables_01.showVegetablesName();
+        Vegetables_01.showVegetablesPrice();
+    }
+    
+}
